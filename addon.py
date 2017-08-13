14d80400e294313e416284cd6a721b01d6e16655
Cristian Asenjo <cristian.a.asenjo@gmail.com>
2017-08-13 09:57:19 -0400

    Detect status of TV power & input

--- addon.py
+++ addon.py
@@ -1,11 +1,33 @@
-import xbmcaddon
+import os
+import sys
+import xbmc
 import xbmcgui
- 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
- 
-line1 = "Hello World!"
-line2 = "We can write anything we want here"
-line3 = "Using Python"
- 
-xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+import xbmcaddon
+import requests
+
+from lib import braviarc
+
+braviarc = braviarc.BraviaRC()
+pin = u'5285'
+braviarc.connect(pin, u'koditvwakeup', u'Kodi')
+
+dialog = xbmcgui.Dialog()
+
+xbmc.executebuiltin('ActivateScreensaver')
+
+if braviarc.get_power_status() == u'standby':
+    dialog.notification('test', 'turning on')
+    braviarc.turn_on()
+else:
+    playing_content = braviarc.get_playing_info()
+    if playing_content.get('title') != u'HDMI 1':
+        braviarc.select_source('HDMI 1')
+    # dialog.notification('test', 'turning off')
+    # braviarc.turn_off()
+
+
+
+
+
+
+
