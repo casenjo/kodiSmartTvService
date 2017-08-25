1aba41a6689d4208a14f3c268387ec7e6b831d70
Cristian Asenjo <cristian.a.asenjo@gmail.com>
2017-08-25 19:08:00 -0400

    Start moving BraviaRC to Sony TV class

--- lib/tv/sony.py
+++ lib/tv/sony.py
@@ -1,6 +1,16 @@
+from ..braviarc import BraviaRC
 
 
 class TvSony: # implements TV contract
 
+    def __init__(self, ipAddress='', macAddress=''):
+        self.ipAddress = ipAddress
+        self.macAddress = macAddress
+        self.tv = BraviaRC(self.tvIp, self.tvMacAddress)
+
     def getName(self):
         return "Sony TV here"
+
+    def getInput(self):
+        playing_content = self.tv.get_playing_info()
+        return playing_content.get('title')
--- lib/tvconnectionmanager.py
+++ lib/tvconnectionmanager.py
@@ -1,5 +1,4 @@
 import utils
-from braviarc import BraviaRC
 
 
 class TvConnectionManager:
