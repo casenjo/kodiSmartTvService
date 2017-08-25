0914ffabc929dce842ddeda926cdd19224eb8a78
Cristian Asenjo <cristian.a.asenjo@gmail.com>
2017-08-25 19:08:33 -0400

    Add isOff method to Sony TV

--- lib/tv/sony.py
+++ lib/tv/sony.py
@@ -14,3 +14,6 @@ class TvSony: # implements TV contract
     def getInput(self):
         playing_content = self.tv.get_playing_info()
         return playing_content.get('title')
+
+    def isOff(self):
+        return self.tv.get_power_status() == u'standby'
